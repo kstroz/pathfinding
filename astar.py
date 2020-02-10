c435467e71be8f3112533386a62dbb221d453d40
kstroz <kstroz24@gmail.com>
2020-02-10 19:18:14 +0100

    Created control panel with starting script button, clearing map button and label which is showing what user is currently doing (drawing walls, clearing them or simply doing nothing)

--- astar.py
+++ astar.py
@@ -12,6 +12,9 @@ class AStar(tk.Frame):
         self.current_color = "white"
         self.color_flag = False
 
+        # Variable for context
+        self.context = tk.StringVar(value="Doing nothing")
+
         # Create map with size of x == width and y == height, weights are used to make this map responsive for all
         # resizing user is doing with the main window
         for row in range(self.width):
@@ -25,23 +28,41 @@ class AStar(tk.Frame):
                 btn.bind("<Enter> ", self.color)
                 btn.grid(row=row, column=column, sticky="nsew")
 
+        # Control Panel
+        self.control_panel = tk.Frame(self, bg="white")
+        self.control_panel.columnconfigure(0, weight=1)
+        self.control_panel.columnconfigure(1, weight=1)
+        self.control_panel.columnconfigure(2, weight=1)
+        self.start_btn = tk.Button(self.control_panel, relief="solid", text="Start", bd=2, bg="white")
+        self.context_lbl = tk.Label(self.control_panel, textvariable=self.context, bg="white")
+        self.clear_btn = tk.Button(self.control_panel, relief="solid", text="Clear map", bd=2, bg="white")
+        self.start_btn.grid(row=0, column=0, sticky="ew")
+        self.context_lbl.grid(row=0, column=1, sticky="ew")
+        self.clear_btn.grid(row=0, column=2, sticky="ew")
+
         # Start point of map
         self.grid_slaves(0, 0)[0].configure(bg="green", activebackground="green")
 
         # End point of map
         self.grid_slaves(self.width - 1, self.height - 1)[0].configure(bg="red", activebackground="red")
 
+        # Control Panel
+        self.control_panel.grid(row=20, columnspan=20, sticky="EW")
+
     def color_trigger(self, event):
         """Method for triggering right color. If scroll is pressed stop coloring map, the left mouse button is used to
         draw walls and the right button is used to clear them. Start and end points cannot be colored or erased"""
         if event.num == 2:
             self.color_flag = False
+            self.context.set("Doing nothing")
         else:
             self.color_flag = True
             if event.num == 1:
                 self.current_color = "black"
+                self.context.set("Drawing walls")
             elif event.num == 3:
                 self.current_color = "white"
+                self.context.set("Clearing walls")
 
         # Color currently clicked Node, its added because color function works only when you entering Node and not when
         # you are clicking on it.
